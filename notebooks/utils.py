0a50ce00f77f9e07f42c61676f93233312268a4c
Abhishek-Sawalkar <abhikuroko@gmail.com>
2021-10-05 19:47:14 +0000

    eda notebook and some changes in dataset

---
+++ notebooks/utils.py
@@ -0,0 +1,6 @@
+Y_2012 = "input/olympic-games"
+Y_2016 = "input/d/rio2016/olympic-games"
+Y_2020 = "input/tokyo-2020-olympics"
+
+print(f'{Y_2012} , {Y_2016}, {Y_2020}')
+
--- src/dataset.py
+++ src/dataset.py
@@ -4,8 +4,10 @@ import os
 
 from utils import * 
 
-df_2012 = pd.DataFrame(pd.read_csv(os.path.join(Y_2012, "summer.csv")))
-df_2012 = pd.DataFrame(pd.read_csv(os.path.join(Y_2012, "winter.csv")))
+df_2012_summer = pd.DataFrame(pd.read_csv(os.path.join(Y_2012, "summer.csv")))
+df_2012_winter = pd.DataFrame(pd.read_csv(os.path.join(Y_2012, "winter.csv")))
+df_2012 = pd.concat([df_2012_summer, df_2012_winter])
+df_2012 = df_2012.drop('Athlete', axis=1)
 df_2016 = pd.DataFrame(pd.read_csv(os.path.join(Y_2016, "athletes.csv")))
 df_2020 = pd.DataFrame(pd.read_csv(os.path.join(Y_2020, "athletes.csv")))
 
